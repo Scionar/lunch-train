7d427d411347f7f7398187956e181ed213faf26b
Joona Viertola <joona.viertola@houston-inc.com>
2017-12-04 11:07:08 +0200

    Update login and leave train user action. Remove old testing.

--- client/src/helpers/user-action/leave-train.js
+++ client/src/helpers/user-action/leave-train.js
@@ -1,16 +1,16 @@
 import socket from '../../websocket';
 import updateAllTrains from '../update-state/update-all-trains';
 
-const joinTrain = (uid, trainId, callback) => {
+const leaveTrain = (uid, trainId, callback) => {
   socket
     .get()
     .emit(
       'server:update:joinedStatus',
-      { joinStatus: true, uid, trainId },
+      { joinStatus: false, uid, trainId },
       () => {
         updateAllTrains(callback);
       }
     );
 };
 
-export default joinTrain;
+export default leaveTrain;
