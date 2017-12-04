694d1ea55bf8ed2f9fadb0b7503155a997c51443
Joona Viertola <joona.viertola@houston-inc.com>
2017-12-04 11:23:22 +0200

    Removed old helpers and bad tests.

--- client/src/helpers/get-all-trains.js
+++
@@ -1,6 +0,0 @@
-const getAllTrains = testData => {
-  const data = testData !== undefined ? testData : [];
-  return data;
-};
-
-export default getAllTrains;
--- client/src/helpers/get-logged-in-status.js
+++
@@ -1,3 +0,0 @@
-const getLoggedInStatus = state => !!state.user;
-
-export default getLoggedInStatus;
