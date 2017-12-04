f7012205ae4608f84ad47bfa0ba4cee2604836fa
Joona Viertola <joona.viertola@houston-inc.com>
2017-12-04 13:56:23 +0200

    Personalize train data at server side

---
+++ api-server/helpers/personalize-train-data.js
@@ -0,0 +1,28 @@
+const getAllJoinedStatuses = require('./get-all-joined-statuses');
+
+/**
+ * Personalize user data.
+ * - Add joined status to joined trains.
+ *
+ * If uid is not defined, no personalization is done.
+ *
+ * @param {Object[]} trains Trains from database.
+ * @param {string} [uid] User ID.
+ *
+ * @return {Object[]} All lunch train objects.
+ */
+module.exports = (trains, uid) => {
+  if (uid !== undefined) {
+    return getAllJoinedStatuses(uid)
+      .then(result => result.map(join => join.trainId)) // Flat joined statuses to an array of train IDs.
+      .then(joined =>
+        trains.map(train => {
+          // Change joined status if current train in array.
+          const trainId = train._id.toString();
+          if (joined.includes(trainId)) train.joined = true;
+          return train;
+        })
+      );
+  }
+  return Promise.resolve(trains);
+};
