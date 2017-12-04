9247bbb4221be33f42bda0c622566cd3f497b0d8
Joona Viertola <joona.viertola@houston-inc.com>
2017-12-04 12:17:31 +0200

    Update all trains to state

---
+++ client/src/helpers/update-state/update-all-trains.js
@@ -0,0 +1,14 @@
+import store from '../../store';
+import { updateTrainsAction } from '../../actions/update-trains';
+import socket from '../../websocket';
+
+const updateAllTrains = callback => {
+  const userState = store.get().getState().user;
+  const uid = userState ? userState.uid : undefined;
+  socket.get().emit('server:get:allTrains', { uid: uid }, result => {
+    store.get().dispatch(updateTrainsAction(result));
+    if (callback) callback();
+  });
+};
+
+export default updateAllTrains;
